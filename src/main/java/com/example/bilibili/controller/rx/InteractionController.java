71e26f5921ac39e513555d725ed9b6e984479f89
浪漫的飞猪 <1447374773@qq.com>
2023-12-21 21:15:46 +0800

    修改

--- src/main/java/com/example/bilibili/controller/rx/InteractionController.java
+++ src/main/java/com/example/bilibili/controller/rx/InteractionController.java
@@ -2,10 +2,7 @@ package com.example.bilibili.controller.rx;
 
 import com.example.bilibili.service.rx.InteractionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -21,6 +18,70 @@ public class InteractionController {
     @ResponseBody
     public List<Map<String, Object>> getCommentsAndRepliesByResourceId(Integer id){
         return interactionService.getCommentsAndRepliesByResourceId(id);
+    }
+    @RequestMapping("/commentByResourseAndUserId")
+    @ResponseBody
+    public String commentByResourseAndUserId(@RequestParam Integer resourceID,
+                                             @RequestParam Integer userID,
+                                             @RequestParam String comment){
+        int result = interactionService.commentByResourseAndUserId(resourceID,userID,comment);
+        if(result==1){
+            return "评论发布成功";
+        }else{
+            return "评论发布失败";
+        }
+    }
+
+    //    回复评论
+    @RequestMapping("/replyForCommentByUserId")
+    @ResponseBody
+    public String replyForCommentByUserId(@RequestParam Integer commentID,
+                                          @RequestParam Integer userID,
+                                          @RequestParam String reply){
+        int result = interactionService.replyForCommentByUserId(commentID,userID,reply);
+        if(result==1){
+            return "回复发布成功";
+        }else{
+            return "回复发布失败";
+        }
+    }
+
+    //    回复回复
+    @RequestMapping("/replyForReplyByUserId")
+    @ResponseBody
+    public String replyForReplyByUserId(@RequestParam Integer replyID,
+                                        @RequestParam Integer userID,
+                                        @RequestParam String reply){
+        int result = interactionService.replyForReplyByUserId(replyID,userID,reply);
+        if(result==1){
+            return "回复成功";
+        }else{
+            return "回复失败";
+        }
+    }
+
+    //    点赞评论
+    @RequestMapping("/addLikeForComment")
+    @ResponseBody
+    public String addLikeForComment(@RequestParam Integer id){
+        int result = interactionService.addLikeForComment(id);
+        if(result>=1){
+            return "succeed";
+        }else{
+            return "fail";
+        }
+    }
+
+    //    点赞回复
+    @RequestMapping("/addLikeForReply")
+    @ResponseBody
+    public String addLikeForReply(@RequestParam Integer id){
+        int result = interactionService.addLikeForReply(id);
+        if(result>=1){
+            return "succeed";
+        }else{
+            return "fail";
+        }
     }
 
 }
--- src/main/java/com/example/bilibili/entity/Upload.java
+++ src/main/java/com/example/bilibili/entity/Upload.java
@@ -2,7 +2,6 @@ package com.example.bilibili.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
