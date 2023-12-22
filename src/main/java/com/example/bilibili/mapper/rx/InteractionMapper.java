41749ec25e79d5bc5dfecb8a3f9e77c6ad1e8e61
smartalmond <3062821768@qq.com>
2023-12-22 12:24:00 +0800

    修修改改

--- src/main/java/com/example/bilibili/mapper/rx/InteractionMapper.java
+++ src/main/java/com/example/bilibili/mapper/rx/InteractionMapper.java
@@ -37,7 +37,7 @@ public interface InteractionMapper {
     int commentByResourseAndUserId(Integer resourceID, Integer userID, String comment);
 
     @Insert("INSERT INTO reply (content, user_id, comment_id,like_num, user_name)" +
-            "VALUES (#{reply}, #{userID}, #{commentID} ,0,0,(SELECT user_name FROM user WHERE id = #{userID} LIMIT 1))")
+            "VALUES (#{reply}, #{userID}, #{commentID} ,0,(SELECT user_name FROM user WHERE id = #{userID} LIMIT 1))")
     int replyForCommentByUserId(Integer commentID, Integer userID, String reply);
 
 
